d19112d7c94c5d7eb2c7214fbc536fba75ce4aae
Samuel Oberger Rockenbach <samuel.rockenbach@univates.br>
2025-02-28 13:50:17 -0300

    Fix AuthController

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -6,9 +6,10 @@ use Rockberpro\RestRouter\Jwt;
 use Rockberpro\RestRouter\Response;
 use Rockberpro\RestRouter\Server;
 use Rockberpro\RestRouter\Utils\DotEnv;
+use Rockberpro\RestRouter\Request;
+use Rockberpro\RestRouter\Controllers\Controller;
 use Rockberpro\RestRouter\Database\Models\SysApiTokens;
 use Rockberpro\RestRouter\Database\Models\SysApiUsers;
-use Rockberpro\RestRouter\Request;
 use Exception;
 
 /**
@@ -16,7 +17,7 @@ use Exception;
  * 
  * @package Rockberpro\RestRouter
  */
-class AuthController
+class AuthController extends Controller
 {
     /**
      * Refresh token
@@ -27,7 +28,7 @@ class AuthController
     public function refresh(Request $request)
     {
         if (DotEnv::get('API_AUTH_METHOD') != 'JWT') {
-            Response::json(['message' => 'Invalid auth method'], Response::UNAUTHORIZED);
+            $this->response(['message' => 'Invalid auth method'], Response::UNAUTHORIZED);
         }
 
         $username = $request->get('username');
@@ -36,10 +37,10 @@ class AuthController
         $sysApiUsers = new SysApiUsers();
         $user = $sysApiUsers->get($username);
         if (!$user) {
-            Response::json(['message' => 'User does not exist'], Response::UNAUTHORIZED);
+            $this->response(['message' => 'User does not exist'], Response::UNAUTHORIZED);
         }
         if (!hash_equals($user->password, hash('sha256', $password))) {
-            Response::json(['message' => 'Invalid credentials'], Response::UNAUTHORIZED);
+            $this->response(['message' => 'Invalid credentials'], Response::UNAUTHORIZED);
         }
 
         $sysApiTokens = new SysApiTokens();
@@ -53,10 +54,10 @@ class AuthController
             $sysApiTokens->add($refresh_token, $user->audience);
         }
         catch (Exception $e) {
-            Response::json(['message' => $e->getMessage()], Response::INTERNAL_SERVER_ERROR);
+            $this->response(['message' => $e->getMessage()], Response::INTERNAL_SERVER_ERROR);
         }
 
-        Response::json([
+        $this->response([
             'refresh-token' => "Bearer {$refresh_token}"
         ], Response::OK);
     }
@@ -70,27 +71,27 @@ class AuthController
     public function access()
     {
         if (DotEnv::get('API_AUTH_METHOD') != 'JWT') {
-            Response::json(['message' => 'Invalid auth method'], Response::UNAUTHORIZED);
+            $this->response(['message' => 'Invalid auth method'], Response::UNAUTHORIZED);
         }
 
         /** validate */
         if (!Server::authorization()) {
-            Response::json(['message' => 'Refresh-token not provided'], Response::BAD_REQUEST);
+            $this->response(['message' => 'Refresh-token not provided'], Response::BAD_REQUEST);
         }
 
         $token = explode(' ', Server::authorization())[1];
         $sysApiTokens = new SysApiTokens();
         if (!$sysApiTokens->exists($token)) {
-            Response::json(['message' => 'Token is invalid'], Response::UNAUTHORIZED);
+            $this->response(['message' => 'Token is invalid'], Response::UNAUTHORIZED);
         }
         if ($sysApiTokens->isRevoked($token)) {
-            Response::json(['message' => 'Token revoked'], Response::UNAUTHORIZED);
+            $this->response(['message' => 'Token revoked'], Response::UNAUTHORIZED);
         }
 
         Jwt::validate(Server::authorization(), 'refresh');
 
         $access_token = Jwt::getAccessToken();
-        Response::json([
+        $this->response([
             'access-token' => "Bearer {$access_token}"
         ], Response::OK);
     }
