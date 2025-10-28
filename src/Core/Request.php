c9fa275367159b2464de355e13e2dbd13d77bef4
Rockberpro <rockberpro@gmail.com>
2025-10-27 23:43:52 -0300

    fix: remove json_encode from log

--- src/Core/Request.php
+++ src/Core/Request.php
@@ -272,7 +272,7 @@ class Request implements RequestInterface
             'user_agent' => Server::userAgent(),
             'request_method' => Server::requestMethod(),
             'request_uri' => Server::requestUri(),
-            'request_body' => json_encode($request->getParams()),
+            'request_body' => $request->getParams(),
             'endpoint' => $request->getAction()->getUri() ?? '',
         ];
         if (!$is_closure) {
