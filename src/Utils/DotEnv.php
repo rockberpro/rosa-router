3fe2874d1a30bb60baf6d87003f3c304c6b5a548
Rockberpro <rockberpro@gmail.com>
2025-10-29 23:58:17 -0300

    feat: implement Sf DotEnv

--- src/Utils/DotEnv.php
+++ src/Utils/DotEnv.php
@@ -2,7 +2,6 @@
 
 namespace Rockberpro\RestRouter\Utils;
 
-use Exception;
 use Throwable;
 
 /**
@@ -12,30 +11,41 @@ use Throwable;
  */
 class DotEnv
 {
+    private static \Symfony\Component\Dotenv\Dotenv $dotEnv;
+
     public static function load($path)
     {
-        try
-        {
-            $env = parse_ini_file($path);
+        try {
+            $dotEnv = self::getInstance();
+            $dotEnv->load($path);
         }
-        catch(Throwable $th)
-        {
-            throw new Exception($th->getMessage());
+        catch(Throwable $th) {
+            throw new DotEnvException($th->getMessage());
         }
-
-        foreach($env as $key => $value)
-        {
-            self::put($key, $value);
-        }
-    }
-
-    private static function put($key, $value)
-    {
-        putenv("$key=$value");
     }
 
     public static function get($key)
     {
-        return getenv($key);
+        $value = getenv($key);
+        if ($value === false) {
+            throw new DotEnvException("The environment variable '{$key}' was not found.");
+        }
+        $v = strtolower(trim($value));
+        if (in_array($v, ['1', 'true', 'on', 'yes', 'y'], true)) {
+            return true;
+        }
+        if (in_array($v, ['0', 'false', 'off', 'no', 'n', ''], true)) {
+            return false;
+        }
+    }
+
+    private static function getInstance(): \Symfony\Component\Dotenv\Dotenv
+    {
+        if (!isset(self::$dotEnv)) {
+            self::$dotEnv = new \Symfony\Component\Dotenv\Dotenv();
+        }
+        return self::$dotEnv;
     }
 }
+
+final class DotEnvException extends \RuntimeException {}
