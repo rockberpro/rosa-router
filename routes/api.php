54f87cf90f3308ebf8f66024da65d49af22eead3
Rockberpro <rockberpro@gmail.com>
2025-10-31 22:29:38 -0300

    feat: demo routes for api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,22 @@
 <?php
 
+use Rockberpro\RestRouter\Core\Response;
 use Rockberpro\RestRouter\Core\Route;
 use Rockberpro\RestRouter\Controllers\AuthController;
+
+Route::get('/status', function() {
+  return new Response([
+        'message' => 'API is running'
+    ], Response::OK);
+});
+
+Route::head('/', function() {
+    return new Response([], Response::OK);
+});
+
+Route::options('/', function() {
+    return new Response();
+});
 
 Route::prefix('auth')->group(function() {
     Route::post('/refresh', [
