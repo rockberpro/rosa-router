82dad363f18bf84813c680d9943186b66fd689c1
Rockberpro <rockberpro@gmail.com>
2025-11-06 22:35:29 -0300

    fix: DotEnv

--- src/Utils/DotEnv.php
+++ src/Utils/DotEnv.php
@@ -13,11 +13,28 @@ class DotEnv
 {
     private static \Symfony\Component\Dotenv\Dotenv $dotEnv;
 
-    public static function load($path)
+    /**
+     * Load the .env file. By default this will enable putenv() (so getenv() returns values)
+     * and will overload existing environment variables. If you want the original
+     * non-overriding behaviour, pass $override = false and $usePutenv = false.
+     *
+     * @param string $path
+     * @param bool $override Whether to overwrite existing environment vars (defaults to true)
+     * @param bool $usePutenv Whether to call putenv() so getenv() reflects the values (defaults to true)
+     */
+    public static function load($path, bool $override = true, bool $usePutenv = true)
     {
         try {
             $dotEnv = self::getInstance();
-            $dotEnv->load($path);
+            if ($usePutenv) {
+                $dotEnv->usePutenv(true);
+            }
+
+            if ($override) {
+                $dotEnv->overload($path);
+            } else {
+                $dotEnv->load($path);
+            }
         }
         catch(Throwable $th) {
             throw new DotEnvException($th->getMessage());
